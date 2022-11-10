375568ea00a058ed885b940418a1b6e31f26a9e9
Zyahara Yananda Hapizh <115675132+ZyaharaARA@users.noreply.github.com>
2022-11-10 20:19:34 +0700

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,28 @@
+function hapusGambar(id) {
+  const hapus = document.getElementById(id);
+  hapus.remove();
+  const count = document.getElementById("basket").children.length - 1;
+  document.getElementById("count").innerHTML = count;
+}
+
+function deleteAndAdd(id, src) {
+  const image = document.createElement("img");
+  image.setAttribute("id", "img1");
+  image.setAttribute("onclick", "hapusGambar(id)");
+  image.src = `${src}`;
+  document.getElementById("basket").appendChild(image);
+  const count = document.getElementById("basket").children.length - 1;
+  document.getElementById("count").innerHTML = count;
+} 
+ 
+function start()
+{
+  var inputColor = prompt("Input your background color");
+  document.body.setAttribute("style","background-color: "+ inputColor);
+}
+
+function start2()
+{
+  var inputColor = prompt("Input your text color");
+  document.getElementById("text1").setAttribute("style","color: "+ inputColor);
+}
